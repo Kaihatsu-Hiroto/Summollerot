0b3831578988427d17ba50d3916007dc0871f60e
DA\nwuser <noppo.kappa@gmail.com>
2018-08-28 19:14:38 +0900

    【ルーレット】一応プロト

---
+++ Summollerot/Assets/SandBox/Kaihatsu/Script/Roulette.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roulette : MonoBehaviour {
+
+    [SerializeField][Range(1f,5f)]
+    private float m_angleSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.eulerAngles += new Vector3(0,0,m_angleSpeed);
+    }
+}
