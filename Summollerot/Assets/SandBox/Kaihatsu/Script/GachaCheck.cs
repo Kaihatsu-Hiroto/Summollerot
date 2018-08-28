9b93020ef78e47357599184bbfec8b599706ca9f
DA\nwuser <noppo.kappa@gmail.com>
2018-08-29 03:25:38 +0900

    【ルーレット】停止ボタンなど設置

--- Summollerot/Assets/SandBox/Kaihatsu/Script/GachaCheck.cs
+++ Summollerot/Assets/SandBox/Kaihatsu/Script/GachaCheck.cs
@@ -6,5 +6,14 @@ public class GachaCheck : MonoBehaviour {
 
     public void OnTriggerEnter2D(Collider2D arg_col){
         Debug.Log(arg_col.tag);
+        if (arg_col.tag == "N"){
+            Debug.Log("ノーマル");
+        }
+        if (arg_col.tag == "R"){
+            Debug.Log("レア");
+        }
+        if (arg_col.tag == "SR"){
+            Debug.Log("スーパーレア");
+        }
     }
 }
