77b5b62291ef34992d71be6db3b2a3c685128858
DA\nwuser <noppo.kappa@gmail.com>
2018-08-28 17:36:48 +0900

    【Player】ジャンプ実装

--- Summollerot/Assets/SandBox/Kaihatsu/Script/Player.cs
+++ Summollerot/Assets/SandBox/Kaihatsu/Script/Player.cs
@@ -8,11 +8,16 @@ public class Player : MonoBehaviour {
     [SerializeField][Range(0.1f,0.5f)]
     private float m_speed;
 
+    [SerializeField][Range(300f,500f)]
+    private float m_jumpPower;
+
     private Vector3 m_playerSpeed;
 
     private SpriteRenderer m_spriteRenderer;
 
     private Rigidbody2D m_rigidbody2D;
+
+    private bool m_isJumping;
 
 	// Use this for initialization
 	void Start () {
@@ -38,7 +43,18 @@ public class Player : MonoBehaviour {
             transform.position -= m_playerSpeed;
             m_spriteRenderer.flipX = true;
         }
-        
 
+        if (Input.GetKeyDown(KeyCode.Space)){
+            if (m_isJumping){
+                m_rigidbody2D.AddForce(transform.up * m_jumpPower);
+                m_isJumping = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D arg_col){
+        if (arg_col.collider.tag == "Ground"){
+            m_isJumping = true;
+        }
     }
 }
