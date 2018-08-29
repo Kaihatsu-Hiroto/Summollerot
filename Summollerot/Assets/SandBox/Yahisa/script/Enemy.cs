9e77575ce88c5c021b4956b8f0b63ee0fda00396
GM2170155\nwuser <minato.nekurochino@gmail.com>
2018-08-29 14:06:38 +0900

    こうもりの移動

---
+++ Summollerot/Assets/SandBox/Yahisa/script/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    //こうもり横移動
+
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = new Vector2(Mathf.PingPong(Time.time, 3), transform.position.y);
+
+    }
+}
